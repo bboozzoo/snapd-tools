053b4e2657eb6d4566961a1f8925c315ce65b87d
Maciek Borzecki <maciek.borzecki@gmail.com>
2017-12-14 20:29:56 +0100

    mostly done
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- analyze.py
+++ analyze.py
@@ -9,17 +9,24 @@ import logging
 
 log = logging.getLogger('analyzer')
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='travis log analyzer')
     parser.add_argument('--top', help='show top 10 tasks',
                         action='store_true', default=False)
     parser.add_argument('--summarize', help='summarize by machine type',
                         action='store_true', default=False)
+    parser.add_argument('--type', help='task type', dest='typ',
+                        choices=('test', 'project', 'any'),
+                        default='any')
     parser.add_argument('-d', '--debug', help='debugging',
+                        action='store_true', default=False)
+    parser.add_argument('-q', '--quiet', help='quiet',
                         action='store_true', default=False)
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
                         default=sys.stdin)
     return parser.parse_args()
+
 
 class TravisInfo:
     id = ""
@@ -28,11 +35,32 @@ class TravisInfo:
     machine = "unknown"
     action = ''
     error = ''
+    typ = '<none>'
+    test = '<none>'
+    line_num = 0
 
     def __str__(self):
-        return '{}: {} {} "{}" took: {} {}'.format(self.id, self.action,
-                                                   self.test if self.test else self.project,
-                                                   self.info, self.duration, self.machine)
+        return '{}: {} {} {} "{}" took: {} {}'.format(self.id, self.action,
+                                                      self.typ, self.test,
+                                                      self.info, self.duration, self.machine)
+
+    def from_info(self, info):
+        self.machine = info['system']
+        self.action = info['action'].lower()
+        self.error = info['error']
+        if info['project']:
+            self.typ = 'project'
+        if info['test']:
+            self.typ = 'test'
+            self.test = info['test']
+
+    def set_line_location(self, line_num, text):
+        self.line_num = line_num
+        self.info = text
+
+    def line(self):
+        return "{}:'{}'".format(self.line_num, self.info)
+
 
 def extract_spread_info(line):
 
@@ -50,15 +78,16 @@ def extract_spread_info(line):
 
     return None
 
+
 def scan(infile):
     info_map = {}
     id = ""
 
-    for line in infile:
-        line = line.strip().replace("\x1b[0K","")
+    for num, line in enumerate(infile):
+        line = line.strip().replace("\x1b[0K", "")
         if line.startswith("travis_time:start:"):
             id = line.split(":")[2]
-            if not id in info_map:
+            if id not in info_map:
                 log.debug('-- new travis info id %s', id)
                 ti = TravisInfo()
                 ti.id = id
@@ -96,12 +125,8 @@ def scan(infile):
                     continue
                 else:
                     # otherwise keep the first line of a fold only
-                    ti.info = line
-                    ti.machine = info['system']
-                    ti.action = info['action'].lower()
-                    ti.error = info['error']
-                    ti.project = info['project']
-                    ti.test = info['test']
+                    ti.from_info(info)
+                    ti.set_line_location(num, line)
                     log.debug('-- fill starting travis info %s', ti)
 
     return info_map
@@ -112,24 +137,25 @@ def output_sorted_by_duration(info_map, n=sys.maxsize):
     i = 0
     for k in sorted_by_duration:
         ti = info_map[k]
-        print(ti.machine, ti.duration, ti.info[:120]+"...\n\n")
+        print(ti.machine, ti.duration, ti.typ, ti.action, ti.line())
         if i > n:
             break
         i += 1
 
 
-def summarize_by_machine(info_map):
+def summarize_by_machine(info_map, typ):
     sorted_by_machine = sorted(info_map, key=lambda k: info_map[k].machine)
     info_map_for_machine = {}
-    current = {}
-    machine = "unknown"
     for k in sorted_by_machine:
         ti = info_map[k]
-        if machine != ti.machine:
-            if len(current) > 0:
-                info_map_for_machine[machine] = current
-            machine = ti.machine
-            current = {}
+        log.debug('ti typ: %s line: "%s"', ti.typ, ti.line())
+        if typ != 'any' and ti.typ != typ:
+            continue
+
+        if ti.machine not in info_map_for_machine:
+            info_map_for_machine[ti.machine] = {}
+
+        current = info_map_for_machine[ti.machine]
         current[k] = ti
     return info_map_for_machine
 
@@ -141,20 +167,13 @@ def output_total_time(descr, info_map):
     print("total time for {}: {}".format(descr, total))
 
 
-if __name__ == "__main__":
-
-    opts = parse_arguments()
-
-    logging.basicConfig(level=logging.DEBUG if opts.debug else logging.INFO)
+def main(opts):
     info_map = scan(opts.infile)
-    # for k, v in info_map.items():
-    #     print(v)
 
     output_total_time("total", info_map)
-    #output_sorted_by_duration(info_map)
 
     if opts.summarize or opts.top:
-        info_map_for_machine = summarize_by_machine(info_map)
+        info_map_for_machine = summarize_by_machine(info_map, opts.typ)
 
     if opts.summarize:
         for k, machine_info_map in info_map_for_machine.items():
@@ -163,5 +182,20 @@ if __name__ == "__main__":
     if opts.top:
         print("\n\ntop-10 for each machine:\n")
         for k, machine_info_map in info_map_for_machine.items():
+            print('{}:\n'.format(k))
             output_sorted_by_duration(machine_info_map, 10)
             print("------------------------------------------------------\n\n")
+
+
+if __name__ == "__main__":
+
+    opts = parse_arguments()
+
+    lvl = logging.INFO
+    if opts.quiet:
+        lvl = logging.WARNING
+    else:
+        lvl = logging.DEBUG if opts.debug else logging.INFO
+    logging.basicConfig(level=lvl)
+
+    main(opts)
